20331fb2b23891d7a781ca02bf48eb444d4a3045
Jacob <obruza@gmail.com>
2016-05-18 22:05:31 -0500

    added more tests

--- test/runecord-test.js
+++ test/runecord-test.js
@@ -1,12 +1,18 @@
 /**
  * Required Dependencies
  */
+const dotenv = require("dotenv");
 const Discord = require("discord.js");
 const chalk = require("chalk");
+const request = require("request");
 const client = new Discord.Client();
 
 const clk = new chalk.constructor({
     enabled: true
+});
+
+dotenv.load({
+    path: ".env"
 });
 
 cYellow = clk.bold.yellow;
@@ -30,18 +36,30 @@ console.log(cYellow("Beginning Bot Test"));
 
 section("Logging In");
 
-client.on("ready", function() {
+client.on("ready", () => {
     pass("Received ready");
 
     exit();
 });
 
-client.loginWithToken(process.env.TOKEN).then(function() {
+client.loginWithToken(process.env.TOKEN).then(() => {
     pass("Valid login token");
 }).catch(function(e) {
     err("Bad token");
     console.log("Error logging in: " + e);
 });
+if (process.env.TWITTER_API) {
+    request("https://cdn.syndication.twimg.com/widgets/timelines/" + process.env.TWITTER_API + "?&lang=en&supress_response_codes=true&rnd=" + Math.random(), (err, res, body) => {
+        if (res.statusCode == 404 || err) {
+            err("Invalid Twitter API");
+            console.log("Twitter API failed: " + err);
+        }
+
+        if (!err && res.statusCode == 200 && body) {
+            pass("Valid Twitter API");
+        }
+    });
+}
 
 function exit() {
     section("Exiting");
