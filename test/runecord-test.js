6eb1ffee19de692a14a5310dffd7f3e5cd93f1e5
Jacob <obruza@gmail.com>
2016-05-19 07:14:47 -0500

    using mocha for testing now

--- test/runecord-test.js
+++ test/runecord-test.js
@@ -1,79 +1,38 @@
-/**
- * Required Dependencies
- */
-const dotenv = require("dotenv");
-const Discord = require("discord.js");
-const chalk = require("chalk");
+const expect = require("chai").expect;
 const request = require("request");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const clk = new chalk.constructor({
-    enabled: true
-});
-
-dotenv.load({
-    path: ".env"
-});
-
-cYellow = clk.bold.yellow;
-cRed = clk.bold.red;
-cGreen = clk.bold.green;
-
-function section(s) {
-    console.log(cYellow("\n    ") + cYellow(s));
-}
-
-function pass(msg) {
-    console.log(cGreen("      ✓ ") + chalk.italic(msg));
-}
-
-function err(msg) {
-    console.log(cRed("      ✗ ") + chalk.italic(msg));
-    process.exit(1);
-}
-
-console.log(cYellow("Beginning Bot Test"));
-
-section("Logging In");
-
-client.on("ready", () => {
-    pass("Received ready");
-
-    exit();
-});
-
-client.loginWithToken(process.env.TOKEN).then(() => {
-    pass("Valid login token");
-}).catch((e) => {
-    err("Bad token");
-    console.log("Error logging in: " + e);
-});
-if (process.env.TWITTER_API) {
-    request("https://cdn.syndication.twimg.com/widgets/timelines/" + process.env.TWITTER_API + "?&lang=en&supress_response_codes=true&rnd=" + Math.random(), (err, res, body) => {
-        if (res.statusCode == 404 || err) {
-            err("Invalid Twitter API");
-            console.log("Twitter API failed: " + err);
-        }
-
-        if (!err && res.statusCode == 200 && body) {
-            pass("Valid Twitter API");
-        }
+describe("Token", () => {
+    it("valid token", () => {
+        client.loginWithToken(process.env.TOKEN).then(() => {
+            done();
+        });
     });
-}
-
-function exit() {
-    section("Exiting");
-
-    client.logout().then(() => {
-        pass("Logged out.");
-        done();
-    }).catch((e) => {
-        err("Couldn't log out: " + e);
+});
+describe("Log In", () => {
+    it("returns ready", () => {
+        client.on("ready", () => {
+            done();
+        });
     });
-}
+});
+describe("Twitter API", () => {
+    var url = "https://cdn.syndication.twimg.com/widgets/timelines/" + process.env.TWITTER_API + "?&lang=en&supress_response_codes=true&rnd=" + Math.random();
 
-function done() {
-    section("Report");
-    pass("All tests done.");
-    process.exit(0);
-}
+    it("returns status 200", () => {
+        /* eslint-disable no-unused-vars */
+        request(url, (error, response, body) => {
+            /* eslint-enable no-unused-vars */
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+});
+describe("Logout", () => {
+    it("logs out", () => {
+        client.logout().then(() => {
+            done();
+        });
+    });
+});
