e12d28576f9c262d4eef58a017e2d5a6067f0dec
Jacob <obruza@gmail.com>
2016-05-22 00:22:03 -0500

    beautified runecord-test.js

--- test/runecord-test.js
+++ test/runecord-test.js
@@ -6,72 +6,72 @@ const client = new Discord.Client();
 const glob = require("glob-all");
 const CLIEngine = require("eslint").CLIEngine;
 const engine = new CLIEngine({
-    envs: ["node", "mocha"],
-    useEslintrc: true
+  envs: ["node", "mocha"],
+  useEslintrc: true
 });
 
 const paths = glob.sync([
-    "./+(bot|test)/**/*.js",
-    "./*.js"
+  "./+(bot|test)/**/*.js",
+  "./*.js"
 ]);
 
 const results = engine.executeOnFiles(paths).results;
 
 describe("ESLint", () => {
-    results.forEach((result) => generateTest(result));
+  results.forEach((result) => generateTest(result));
 });
 
 describe("Token", () => {
-    it("valid token", () => {
-        client.loginWithToken(process.env.TOKEN).then(() => {
-            done();
-        });
+  it("valid token", () => {
+    client.loginWithToken(process.env.TOKEN).then(() => {
+      done();
     });
+  });
 });
 describe("Log In", () => {
-    it("returns ready", () => {
-        client.on("ready", () => {
-            done();
-        });
+  it("returns ready", () => {
+    client.on("ready", () => {
+      done();
     });
+  });
 });
 describe("Twitter API", () => {
-    var url = "https://cdn.syndication.twimg.com/widgets/timelines/" + process.env.TWITTER_API + "?&lang=en&supress_response_codes=true&rnd=" + Math.random();
+  var url = "https://cdn.syndication.twimg.com/widgets/timelines/" + process.env.TWITTER_API + "?&lang=en&supress_response_codes=true&rnd=" + Math.random();
 
-    it("returns status 200", () => {
-        /* eslint-disable no-unused-vars */
-        request(url, (error, response, body) => {
-            /* eslint-enable no-unused-vars */
-            expect(response.statusCode).to.equal(200);
-            done();
-        });
+  it("returns status 200", () => {
+    /* eslint-disable no-unused-vars */
+    request(url, (error, response, body) => {
+      /* eslint-enable no-unused-vars */
+      expect(response.statusCode).to.equal(200);
+      done();
     });
+  });
 });
 describe("Logout", () => {
-    it("logs out", () => {
-        client.logout().then(() => {
-            done();
-        });
+  it("logs out", () => {
+    client.logout().then(() => {
+      done();
     });
+  });
 });
 
 function generateTest(result) {
-    const {
-        filePath,
-        messages
-    } = result;
+  const {
+    filePath,
+    messages
+  } = result;
 
-    it("validates " + filePath, () => {
-        if (messages.length > 0) {
-            assert.fail(false, true, formatMessages(messages));
-        }
-    });
+  it("validates " + filePath, () => {
+    if (messages.length > 0) {
+      assert.fail(false, true, formatMessages(messages));
+    }
+  });
 }
 
 function formatMessages(messages) {
-    const errors = messages.map((message) => {
-        return message.line + ":" + message.column + " " + message.message.slice(0, -1) + " - " + message.ruleId + "\n";
-    });
+  const errors = messages.map((message) => {
+    return message.line + ":" + message.column + " " + message.message.slice(0, -1) + " - " + message.ruleId + "\n";
+  });
 
-    return "\n" + errors.join("");
+  return "\n" + errors.join("");
 }
