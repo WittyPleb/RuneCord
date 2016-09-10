d4cbbd643c16bb4db33477003058bdba89c67ca8
unlucky4ever <obruza@gmail.com>
2016-09-09 19:30:34 -0500

    added raven command

--- bot/commands/user.js
+++ bot/commands/user.js
@@ -812,6 +812,29 @@ var commands = {
 
       msg.channel.sendMessage(toSend);
     }
+  },
+  'raven': {
+    desc: 'Tells you if there is a raven spawned in Prifddinas.',
+    usage: '',
+    process: (client, msg) => {
+      var spawned = false;
+      var daysUntilNext = 0;
+      var formula = (((Math.floor((Date.now() / 1000) / (24 * 60 * 60))) + 7) % 13);
+
+      if (formula < 1) {
+        daysUntilNext = 1 - formula;
+        spawned = true;
+      } else {
+        daysUntilNext = 13 - formula;
+        spawned = false;
+      }
+
+      if (spawned) {
+        msg.channel.sendMessage(`A raven is currently spawned in Prifddinas!`);
+      } else {
+        msg.channel.sendMessage(`There isn\'t a raven in Prifddinas at this time, the next one spawns in **${daysUntilNext}** day${(daysUntilNext > 1 ? 's' : '')}.`);
+      }
+    }
   }
 };
 
