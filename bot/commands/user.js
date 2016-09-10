4041afc966a78025a5fc1a0f89bac9553acdce43
unlucky4ever <obruza@gmail.com>
2016-09-09 19:18:34 -0500

    added spotlight command

--- bot/commands/user.js
+++ bot/commands/user.js
@@ -731,6 +731,54 @@ var commands = {
 
       msg.channel.sendMessage(toSend);
     }
+  },
+  'spotlight': {
+    desc: 'Displays what the current minigame spotlight is.',
+    usage: '',
+    process: (client, msg) => {
+      var minigames = [
+        'Pest Control',
+        'Soul Wars',
+        'Fist of Guthix',
+        'Barbarian Assault',
+        'Conquest',
+        'Fishing Trawler',
+        'The Great Orb Project',
+        'Flash Powder Factory',
+        'Castle Wars',
+        'Stealing Creation',
+        'Cabbage Facepunch Bonanza',
+        'Heist',
+        'Mobilising Armies',
+        'Barbarian Assault',
+        'Conquest',
+        'Fist of Guthix',
+        'Castle Wars',
+        'Pest Control',
+        'Soul Wars',
+        'Fishing Trawler',
+        'The Great Orb Project',
+        'Flash Powder Factory',
+        'Stealing Creation',
+        'Cabbage Facepunch Bonanza',
+        'Heist',
+        'Trouble Brewing',
+        'Castle Wars'
+      ];
+
+      var currentSpotlight = Math.floor((((Math.floor((Date.now() / 1000) / (24 * 60 * 60))) - 49) % (3 * minigames.length)) / 3);
+      var daysUntilNext = 3 - ((Math.floor((Date.now() / 1000) / (24 * 60 * 60))) - 49) % (3 * minigames.length) % 3;
+      var nextSpotlight = currentSpotlight + 1;
+
+      if (nextSpotlight === minigames.length) nextSpotlight = 0; // Resets to the beginning
+
+      var toSend = [];
+      toSend.push(`The current minigame that is on spotlight is **${minigames[currentSpotlight]}**.`);
+      toSend.push(`The next minigame to be on spotlight will be **${minigames[nextSpotlight]}** in **${daysUntilNext}** day${(daysUntilNext > 1 ? 's' : '')}.`);
+      toSend = toSend.join('\n');
+
+      msg.channel.sendMessage(toSend);
+    }
   }
 };
 
