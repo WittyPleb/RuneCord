903b9f9969fdf2ace238972493c1b2474be0d590
unlucky4ever <obruza@gmail.com>
2016-11-04 07:22:16 -0500

    fixed ESLint warnings

--- bot/util/updateCount.js
+++ bot/util/updateCount.js
@@ -1,40 +1,40 @@
-const request = require('request');
-const config  = require('../config.json');
+const request = require(`request`);
+const config  = require(`../config.json`);
 
 module.exports = count => {
   updateCarbon(count);
   updateDiscord(count);
 }
-
+/* eslint no-unused-vars: "off" */
 const updateCarbon = count => {
-  if (process.env.CARON_KEY === '') return; // No carbon key is set
+  if (process.env.CARON_KEY === ``) return; // No carbon key is set
 
-  console.log('Updating Carbon...');
+  console.log(`Updating Carbon...`);
   request.post({
-    'url': 'https://www.carbonitex.net/discord/data/botdata.php',
-    'headers': {'content-type': 'application/json'},
+    'url': `https://www.carbonitex.net/discord/data/botdata.php`,
+    'headers': {'content-type': `application/json`},
     'json': true,
     body: {
       'key': config.stats.carbon,
       'servercount': count
     }
   }, (err, res, body) => {
-    if (!err) console.log('SUCCESSFULL CARBON UPDATE', count);
+    if (!err) console.log(`SUCCESSFULL CARBON UPDATE`, count);
   });
 }
 
 const updateDiscord = count => {
-  if (process.env.DISCORD_BOTS_KEY === '') return; // No Discord.pw key is set
+  if (process.env.DISCORD_BOTS_KEY === ``) return; // No Discord.pw key is set
 
-  console.log('Updating Discord.pw...');
+  console.log(`Updating Discord.pw...`);
   request.post({
     'url': `https://bots.discord.pw/api/bot/${config.bot.user_id}/stats`,
-    'headers': {'content-type': 'application/json'},
+    'headers': {'content-type': `application/json`},
     json: true,
     body: {
       'server_count': count
     }
   }, (err, res, body) => {
-    if (!err) console.log('SUCCESSFULL DISCORD.PW UPDATE', count);
+    if (!err) console.log(`SUCCESSFULL DISCORD.PW UPDATE`, count);
   });
 }
