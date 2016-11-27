80555cfeb456b27c8ee98b355c64268f4eaae678
Witty <obruza@gmail.com>
2016-11-27 17:47:00 -0600

    add viswax cmd

---
+++ commands/user/viswax.js
@@ -0,0 +1,41 @@
+var superagent = require('superagent');
+var reload = require('require-reload');
+var config = require('../../config.json');
+var logger = new (reload('../../utils/Logger.js'))(config.logTimestamp);
+
+module.exports = {
+	desc: 'Get the current Viswax combination.',
+	task(bot, msg) {
+		superagent.get('http://services.runescape.com/m=forum/forums.ws?75,76,387,65763383')
+			.end((error, response) => {
+				if (error) {
+					logger.warn('Error getting Viswax combination: ' + (error.status || error.response));
+					bot.createMessage(msg.channel.id, 'There was an error while grabbing the Viswax combination. Please try again later.').then(sentMsg => {
+						setTimeout(() => { msg.delete(); sentMsg.delete(); }, 10000); // Delete messages after 10 seconds.
+					});
+				} else {
+					let dateMatch = new RegExp(/combination\s+?for.+?(\d+)(?:..)?;/i).exec(response.text)[1];
+					let day = new Date().getUTCDate();
+
+					// Checks if combination is updated
+					if (day != dateMatch) {
+						bot.createMessage(msg.channel.id, "The combination hasn't been updated yet. Please try again later.");
+						return;
+					}
+
+					let match = new RegExp(/slot 1:.+?- (.+?)slot 2:.+?- (.+?)slot/i).exec(response.text);
+					let slot1 = match[1].replace(/<.+?>/g, ``).trim().split(`-`).map(r => r.trim()).join(`, `);
+					let slot2 = match[2].replace(/<.+?>/g, ``).trim().split(`-`).map(r => r.trim()).join(`, `);
+
+					let toSend = [];
+
+					toSend.push(`**First Slot**: ${slot1}`);
+					toSend.push(`**Second Slot**: ${slot2}`);
+
+					toSend = toSend.join('\n');
+
+					bot.createMessage(msg.channel.id, toSend);
+				}
+			});
+	}
+}
