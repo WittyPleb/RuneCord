d9a1e0b28ff6aa48bcb9eb2cf0a57f4c3a241540
Witty <obruza@gmail.com>
2016-11-27 16:47:56 -0600

    added reset command

---
+++ commands/user/reset.js
@@ -0,0 +1,23 @@
+module.exports = {
+	desc: 'Tells you when the game will reset.',
+	task(bot, msg) {
+		let resetTime = new Date().setUTCHours(24, 0, 0, 0) - Date.now();
+		let hours = Math.floor(resetTime / 1000 / 60 / 60);
+		resetTime -= hours * 1000 * 60 * 60;
+
+		let minutes = Math.floor(resetTime / 1000 / 60);
+		resetTime -= minutes * 1000 * 60;
+
+		let timestr = '';
+
+		if (hours > 0) {
+			timestr += `${hours} hour${hours > 1 ? 's' : ''}`;
+		}
+
+		if (minutes > 0) {
+			timestr += ` ${minutes} minute${minutes > 1 ? 's' : ''}`;
+		}
+
+		bot.createMessage(msg.channel.id, `The game will reset in **${timestr}**.`);
+	}
+}
