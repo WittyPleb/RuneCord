6eb0d5d6a74a2931c4386d1c9333aab1cd2f9456
Witty <obruza@gmail.com>
2016-11-27 22:09:14 -0600

    add max int check to roll

--- commands/user/roll.js
+++ commands/user/roll.js
@@ -7,6 +7,10 @@ module.exports = {
 	aliases: ['random'],
 	task(bot, msg, suffix) {
 		let args = suffix.match(/(?:(\d+)-)?(\d+)/);
+		if (args[1] || args[2] >= Number.MAX_SAFE_INTEGER) {
+			bot.createMessage(msg.channel.id, 'That number is too big for me to process, please use a smaller number.');
+			return;
+		}
 		let roll = args === null ? [1, 10] : [parseInt(args[1]) || 1, parseInt(args[2])];
 		bot.createMessage(msg.channel.id, `${msg.author.username} rolled **${Nf.format(roll[0])}-${Nf.format(roll[1])}** and got **${Nf.format(~~((Math.random() * (roll[1] - roll[0] + 1)) + roll[0]))}**.`);
 	}
