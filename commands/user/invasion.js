1ad870fab7136f6ed947ff8f628a9615e9786fa2
Witty <obruza@gmail.com>
2016-11-27 22:31:28 -0600

    add invasion cmd

---
+++ commands/user/invasion.js
@@ -0,0 +1,12 @@
+var Nf = new Intl.NumberFormat('en-US');
+
+module.exports = {
+	desc: 'Tells you how much XP you\'d gain from a fully completed Troll Invasion D&D',
+	aliases: ['troll', 'trollinvasion'],
+	usage: '<level>',
+	task(bot, msg, suffix) {
+		if (!parseInt(suffix)) return 'wrong usage';
+		let formula = 8 * (20 / 20) * (Math.pow(parseInt(suffix), 2) - 2 * parseInt(suffix) + 100);
+		bot.createMessage(msg.channel.id, `If were to **fully** complete Troll Invasion, you'd gain **${Nf.format(formula)}** XP if you were level **${parseInt(suffix)}**.`);
+	}
+}
