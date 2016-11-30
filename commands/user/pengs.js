1c10bd2ed122fb2fb104f004c0860498cf656942
Witty <obruza@gmail.com>
2016-11-30 15:40:02 -0600

    removed unnecessary code

--- commands/user/pengs.js
+++ commands/user/pengs.js
@@ -17,14 +17,12 @@ module.exports = {
 			let pengGp = 6500 * points;
 			bot.createMessage(msg.channel.id, `You'd gain **${Nf.format(pengGp)}** coins, if you use **${points}** points.`);
 			return;
-		} else {
-			if (level < 1 || level > 120) {
-				bot.createMessage(msg.channel.id, 'Please use a number between 1-120 for the level.');
-				return;
-			}
-			let pengXp = 25 * level * points;
-			bot.createMessage(msg.channel.id, `You'd gain **${Nf.format(pengXp)}** XP at level **${level}** if you used **${points}** points.`);
+		}
+		if (level < 1 || level > 120) {
+			bot.createMessage(msg.channel.id, 'Please use a number between 1-120 for the level.');
 			return;
 		}
+		let pengXp = 25 * level * points;
+		bot.createMessage(msg.channel.id, `You'd gain **${Nf.format(pengXp)}** XP at level **${level}** if you used **${points}** points.`);
 	}
 }
