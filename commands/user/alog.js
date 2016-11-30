fd07e71a8f86f216eab3cec3c87ebb68392d2e92
Witty <obruza@gmail.com>
2016-11-29 20:58:02 -0600

    fix unhandledRejection error

--- commands/user/alog.js
+++ commands/user/alog.js
@@ -19,9 +19,7 @@ module.exports = {
 		superagent.get(`http://services.runescape.com/m=adventurers-log/a=13/rssfeed?searchName=${suffix}`).end((error, response) => {
 			if (error) {
 				logger.warn('Error getting stats for a user: ' + (error.status || error.response));
-				bot.createMessage(msg.channel.id, 'There was an error while grabbing the adventurer\'s log for \'' + suffix + '\'. Please try again later.').then(sentMsg => {
-					setTimeout(() => { msg.delete(); sentMsg.delete(); }, 10000); // Delete messages after 10 seconds.
-				});
+				bot.createMessage(msg.channel.id, 'There was an error while grabbing the adventurer\'s log for \'' + suffix + '\'. Please try again later.');
 			} else {
 				let alogBody = response.text;
 				let alogText = alogBody.slice(alogBody.indexOf('<item>'), alogBody.indexOf('</channel>'));
